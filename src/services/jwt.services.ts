1c95dca76ca4235623ac3b718a575dbffe2c3a0e
nicoContigliani <nico.contigliani>
2025-01-26 03:33:53 -0300

    clean

--- src/services/jwt.services.ts
+++ src/services/jwt.services.ts
@@ -3,22 +3,17 @@ import 'dotenv/config'
 
 export const jwtGenerateToken = async (data: any) => {
     let secretKey: any = process.env.JWT_SIGNATURE_GENERAL || "Esto Es Nueva";
-    const token = jwt.sign(data, secretKey, { expiresIn: '10000h' }); // Token expires in 1 hour
+    const token = jwt.sign(data, secretKey, { expiresIn: '10000h' });
     return token
 }
 
 export const jwtCompareToken = async (token: string) => {
     try {
-        // Obtener la clave secreta del archivo .env
         const secretKey: string = process.env.JWT_SIGNATURE_GENERAL || "Esto Es Nueva";
+        const decoded = jwt.verify(token, secretKey);
 
-        // Verificar y decodificar el token
-        const decoded = jwt.verify(token, secretKey); // Devuelve el payload si es válido
-
-        // Si el token es válido, retornar el decoded (payload)
         return decoded;
     } catch (error) {
-        // Si el token no es válido o ha expirado
         console.error("Error with token:", error);
         throw new Error("Token invalid o expired");
     }
